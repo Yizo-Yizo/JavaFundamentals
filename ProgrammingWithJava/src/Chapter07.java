18d9dd280ee6b31f84d5568b39cbbc11da53520b
thapelo <thapelo@thapelo-HP-Laptop-15-bs0xx>
2022-02-06 18:14:18 +0200

    Finished Chapter 7

--- ProgrammingWithJava/src/Chapter07.java
+++ ProgrammingWithJava/src/Chapter07.java
@@ -378,42 +378,556 @@ public class Chapter07 {
 //		String[] suits = {"Spade", "Diamonds", "Clubs", "Heart"};
 //		String[] numbers = {"King", "Queen", "Jack", "Ace", "2", "3", "4", "5", "6", "7", "8", "9"};
 		
-		var input = "0,1,2,3,4,6,7,8,11,12,14,15,16,17,18,19,20,21,22,23,24,25,27,28,29,30,31,32,33,35,36,37,38,39";
-		List<String> numbers = Arrays.asList(input.split(","));
-		//Collections.sort(numbers);
-		List<Integer> sortedNumbers = new ArrayList<Integer>();
-		sortedNumbers.addAll(numbers.stream().map(Integer::valueOf).collect((Collectors.toList())));
-		Collections.sort(sortedNumbers);
+		// Group and print out intervals
+//		var input = "0,1,2,3,4,6,7,8,11,12,14,15,16,17,18,19,20,21,22,23,24,25,27,28,29,30,31,32,33,35,36,37,38,39";
+//		List<String> numbers = Arrays.asList(input.split(","));
+//		//Collections.sort(numbers);
+//		List<Integer> sortedNumbers = new ArrayList<Integer>();
+//		sortedNumbers.addAll(numbers.stream().map(Integer::valueOf).collect((Collectors.toList())));
+//		Collections.sort(sortedNumbers);
+//		
+//		
+//		summarizeCollection(sortedNumbers);
+//		System.out.println(summarizeCollection(sortedNumbers));
+		// 7.25
+//		Scanner input = new Scanner(System.in);
+//		
+//		double[] eqn = new double[3];
+//		double[] roots = new double[2];
+//		
+//		System.out.print("Enter coefficients of your equation: ");
+//		for (int i = 0; i < eqn.length; i++) {
+//			eqn[i] = input.nextDouble();
+//		}
+//		
+//		System.out.println("Number of real roots: "+ solveQuadratic(eqn, roots) +" and roots are: "+ roots[0] +" and "+ roots[1]);
 		
+		//7.26
+//		Scanner input = new Scanner(System.in);
+//		
+//		System.out.print("Enter numbers for the first list: ");
+//		var size = input.nextInt();
+//		int[] list1 = new int[size];
+//		for (int i = 0; i < list1.length; i++) {
+//			list1[i] = input.nextInt();
+//		}
+//		
+//		System.out.print("Enter numbers for the second list: ");
+//		var size2 = input.nextInt();
+//		int[] list2 = new int[size2];
+//		for (int i = 0; i < list2.length; i++) {
+//			list2[i] = input.nextInt();
+//		}
+//		input.close();
+//		System.out.println("Two list are" + (equals(list1,list2) ? " " : " not ") + "strictly identical");
 		
-		summarizeCollection(sortedNumbers);
-		System.out.println(summarizeCollection(sortedNumbers));
+		//7.27
+//		Scanner input = new Scanner(System.in);
+//		
+//		System.out.print("Enter list1: ");
+//		var size = input.nextInt();
+//		int[] list1 = new int[size];
+//		for (int i = 0; i < list1.length; i++) {
+//			list1[i] = input.nextInt();
+//		}
+//		
+//		System.out.print("Enter list2: ");
+//		var size2 = input.nextInt();
+//		int[] list2 = new int[size2];
+//		for (int i = 0; i < list2.length; i++) {
+//			list2[i] = input.nextInt();
+//		}
+//		
+//		input.close();
+//		
+//		System.out.println("Two list are" + (equals(list1,list2) ? " " : " not ") + "identical");
+		
+		//7.28
+//		Scanner input = new Scanner(System.in);
+//		
+//		int[] numbers = new int[10];
+//		
+//		System.out.print("Enter 10 integers: ");
+//		for (int i = 0; i < numbers.length; i++) {
+//			numbers[i] = input.nextInt();
+//		}
+//		
+//		combination(numbers);
+		
+		//7.29
+//		String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+//		int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+//		
+//		System.out.println("We have " + pickFourCards(suit, numbers) + " picks.");
+		
+		//7.30
+//		Scanner input = new Scanner(System.in);
+//		
+//		System.out.println("Enter the number of values: ");
+		
+		//7.31
+//		Scanner input = new Scanner(System.in);
+//		
+//		System.out.print("Enter list1: ");
+//		var size = input.nextInt();
+//		int[] list1 = new int[size];
+//		
+//		for (int i = 0; i < list1.length; i++) {
+//			
+//			list1[i] = input.nextInt();
+//		}
+//		
+//		System.out.print("Enter list2: ");
+//		var size2 = input.nextInt();
+//		int[] list2 = new int[size2];
+//		
+//		for (int i = 0; i < list2.length; i++) {
+//			
+//			list2[i] = input.nextInt();
+//		}
+//		
+//		var newList = merge(list1, list2);
+//		
+//		System.out.print("The merged list is");
+//		for (int i : newList) {
+//			System.out.print(" " + i);
+//		}
+		
+		//7.32
+//		Scanner input = new Scanner(System.in);
+//		
+//		System.out.print("Enter list: ");
+//		var size = input.nextInt();
+//		int[] list = new int[size];
+//		
+//		for (int i = 0; i < list.length; i++) {
+//			list[i] = input.nextInt();
+//		}
+//		
+//		partition(list);
+		
+		//7.33
+//		Scanner input = new Scanner(System.in);
+//		
+//		String[] animals = {"monkey", "rooster", "dog", "pig", "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "sheep"};
+//		
+//		System.out.print("Enter a year: ");
+//		int year = input.nextInt();
+//		
+//		var animal = animals[year % 12];
+//		System.out.println(animal);
+		
+		//7.34
+//		Scanner input = new Scanner(System.in);
+//		
+//		System.out.print("Enter string: ");
+//		var str = input.next();
+//		input.close();
+//		
+//		System.out.println(sort(str));
+		
+		//7.35
+//		Scanner input = new Scanner(System.in);
+//		
+//		String[] words = {"write", "any", "words", "you", "like"};
+//		
+//		Random random = new Random();
+//		
+//		var randomIndex = random.nextInt(words.length);
+//		var randomWord = words[randomIndex];
+//		var wordArray = randomWord.toCharArray();
+//		
+//		String asterisk = asterisk(wordArray);
+//		int index = 0;
+//		int count = 0;
+//		
+//		System.out.print("Enter a letter in word "+ asterisk + " > ");
+//		var letter = input.next().charAt(0);
+//		hangman(letter, wordArray, index, count);
+//		
+//		char[] word = asterisk.toCharArray();
+//		word[index] = letter; 
+//		
+//		System.out.print("Enter a letter in word " + asterisk + " > ");
 	}
+	//7.35
+//		public static void hangman(char c, char[] wordArray, int index, int count){
+//			
+//			
+//			for (int i = 0; i < wordArray.length; i++){
+//				
+//				if (c == wordArray[i]) {
+//					index = i;
+//					break;
+//				}
+//				else
+//					count++;
+//			}
+//			
+//		}
+//		
+//		public static String asterisk(char[] wordArray) {
+//			
+//			String c = "";
+//			char asterisk = '*';
+//			
+//			for (int i = 0; i < wordArray.length; i++) {
+//				c = c + asterisk;
+//			}
+//			
+//			return c;
+//		}
+		
+		//7.34
+//		public static String sort(String s) {
+//			
+//			char[] characters = new char[s.length()];
+//			int i = 0;
+//			
+//			for (char c : s.toCharArray()) {
+//				characters[i] = c;
+//				i++;
+//			}
+//			
+//			for (int j = 0; j < characters.length - 1; j++) {
+//				
+//				var min = characters[j];
+//				var minIndex = j;
+//				
+//				for (int k = j+1; k < characters.length; k++) {
+//					
+//					if (characters[k] < min) {
+//						min = characters[k];
+//						minIndex = k;
+//					}
+//				}
+//				
+//				
+//				if (minIndex != j) {
+//					characters[minIndex] = characters[j];
+//					characters[j] = min;
+//				}
+//			}
+//			
+//			String str = "";
+//			for (char c : characters) {
+//				str = str + c;
+//			}
+//			
+//			return str;
+//		}
+		//7.32
+//		public static int partition(int[] list) {
+//			
+//			var index = 0;
+//			var pivot = list[index];
+//			
+//			var lessThanPivot = lessThanPivot(list);
+//			
+//			System.out.print("After the partition, the list is ");
+//			for (int i : lessThanPivot)
+//				System.out.print(" " + i);
+//			
+//			return index;
+//		}
+	//	
+//		public static int[] lessThanPivot(int[] list) {
+//			
+//			int j = 0;
+//			var index = 0;
+//			var pivot = list[index];
+//			
+//			for (int i = 0; i < list.length; i++) {
+//				
+//				if (list[i] <= pivot) {
+//					index++;
+//				}
+//			}
+//			
+//			int[] temp = new int[index];
+//			
+//			for (int i = 0; i < list.length; i++) {
+//				
+//				if (list[i] < pivot) {
+//					temp[j] = list[i];
+//					j++;
+//				}
+//			}
+//			
+//			temp[index-1] = pivot;
+//			
+//			var greaterThanPivotList = greaterThanPivot(list);
+//			
+//			for (int i = 0; i < temp.length; i++) {
+//				list[i] = temp[i];
+//			}
+//			
+//			int k = 0;
+//			for (int i = temp.length; i < (greaterThanPivotList.length + temp.length); i++) {
+//				list[i] = greaterThanPivotList[k];
+//				k++;
+//			}
+//			
+//			return list;
+//		}
+	//	
+	//	
+//		public static int[] greaterThanPivot(int[] list) {
+//			
+//			var index = 0;
+//			var pivot = list[index];
+//			
+//			for (int i = 0; i < list.length; i++) {
+//				
+//				if (list[i] > pivot) {
+//					index++;
+//				}
+//			}
+//			
+//			int[] temp = new int[index];
+//			int j = 0;
+//			
+//			for (int i = 0; i < list.length; i++) {
+//				
+//				if (list[i] > pivot) {
+//					temp[j] = list[i];
+//					j++;
+//				}
+//			}
+//			
+//			return temp;
+//		}
+		
+		
+		//7.31
+//		public static int[] merge(int[] list1, int[] list2) {
+//			
+//			int lengthOfArray = list1.length + list2.length; 
+//			int[] newList = new int[lengthOfArray];
+//			
+//			for (int i = 0; i < list1.length; i++) {
+//				newList[i] = list1[i];
+//			}
+//			
+//			for (int i = 0, j = list1.length; i < list2.length; i++, j++) {
+//				newList[j] = list2[i];
+//			}
+//			
+//			sortArray(newList);
+//			
+//			return newList;
+//		}
+		
+//		public static void sortArray(int[] newList) {
+//			
+//			for (int i = 0; i < newList.length-1; i++) {
+//				var min = newList[i];
+//				var minIndex = i;
+//				
+//				for (int j =  i+1; j < newList.length; j++) {
+//					
+//					if (newList[j] < min) {
+//						min = newList[j];
+//						minIndex = j;
+//					}
+//				}
+//			
+//				if (minIndex != i) {
+//					newList[minIndex] = newList[i];
+//					newList[i] = min;
+//				}
+//			}
+//		}
+		//7.30
+//		public static boolean isConsecutiveFour(int[] values) {
+//			
+//			int count = 0;
+//			int i = 0;
+//			
+//			while (count != 4 && i != values.length) {
+//				
+//				for (i=1; i < values.length; i++) {
+//					if (values[i] == values[i-1]) {
+//						count++;
+//					}
+//				}
+//				
+//				if (count == 4) {
+//					return true;
+//				}
+//			}
+//			
+//			return false;
+//		}
+		//7.29
+//		public static int pickFourCards(String[] suit, int[] numbers) {
+//			
+//			int sum = 0;
+//			int count = 0;
+//			
+//			Random random = new Random();
+//			HashMap<String, Integer> deckOfCards = new HashMap<String, Integer>();
+//			
+//			while (sum != 24) {
+//				
+//				sum = 0;
+//				for (int i = 0; i <= 4; i++) {
+//					
+//					var randomSuitIndex = random.nextInt(suit.length);
+//					var randomValuesIndex = random.nextInt(numbers.length);
+//					
+//					deckOfCards.put(suit[randomSuitIndex], numbers[randomValuesIndex]);
+//					
+//				}
+//				
+//				for (int i : deckOfCards.values()) {
+//					sum += i;
+//				}
+//				count++;
+//			}
+//			
+//			return count;
+//		}
+		
+		//7.28
+//		public static void combination(int[] list) {
+//			
+//			for (int i = 0; i < list.length; i++) {
+//				
+//				for (int j = 0; j < list.length; j++) {
+//					
+//					System.out.println("(" +list[i]+","+list[j]+ ")");
+//				}
+//			}
+//		}
+		
+		//7.27
+//		public static boolean equals(int[] list1, int[] list2) {
+//			
+//			if (list1.length != list2.length) {
+//				return false;
+//			}
+//			
+//			sort(list1);
+//			sort(list2);
+//			
+//			for (int i = 0; i < list1.length; i++) {
+//				if (list1[i] != list2[i]) {
+//					return false;
+//				}
+//			}
+//			return true;
+//		}
+	//	
+//		public static void sort(int[] array) {
+//			
+//			for (int i = 0; i < array.length; i++) {
+//				var min = array[i];
+//				var minIndex = i;
+//				
+//				for (int j = 0; j < array.length; j++) {
+//					
+//					if (array[j] < min) {
+//						min = array[j];
+//						minIndex = j;
+//					}
+//				}
+//				
+//				if (minIndex != i) {
+//					array[minIndex] = array[i];
+//					array[i] = min;
+//				}
+//			}
+//		}
+		//7.26
+//		public static boolean equals(int[] list1, int[] list2) {
+//			
+//			int counter = 0;
+//			for (int i = 0; i < list1.length; i++) {
+//				
+//				if (list1[i] == list2[i]) {
+//					counter++;
+//				}
+//			}
+//			
+//			if (list1.length == list2.length && counter == list1.length) {
+//				return true;
+//			}
+//			else
+//				return false;
+//		}
+		
+		
+		// 7.25
+//		public static int solveQuadratic(double[] eqn, double[] roots) {
+//			
+//			var d = Math.pow(eqn[1], 2);
+//			var e = 4*eqn[0]*eqn[2];
+//			var f = d-e;
+//			
+//			if (f > 0) {
+//				roots[0] = (-eqn[1] + Math.pow(f, 0.5)) / (2*eqn[0]);
+//				roots[1] = (-eqn[1] - Math.pow(f, 0.5)) / (2*eqn[0]);
+//				return roots.length;
+//			}
+//			else if (f == 0) {
+//				roots[0] = -eqn[1];
+//				return roots.length;
+//			}
+//			else {
+//				return 0;
+//			}
+//		}
+//		public static String summarizeCollection(Collection<Integer> input) {
+//			//ArrayList<Integer> input = new ArrayList<Integer>();
+//			int lengthOfInput = input.size();
+//			int firstIndex = 0, secondIndex = 0;
+//			//String summarizedCollection = "";
+//			StringBuilder summarizedCollection = new StringBuilder();
+//			
+//			while (firstIndex < lengthOfInput) {
+//				while ((++secondIndex < lengthOfInput) && (((List<Integer>) input).get(secondIndex) - ((List<Integer>) input).get(secondIndex -1) == 1));
+//					
+//				if (secondIndex - firstIndex > 2) {
+//					//System.out.printf("%s-%s", ((List<Integer>) input).get(firstIndex), ((List<Integer>) input).get(secondIndex - 1));
+//					summarizedCollection.append(String.format("%s-%s,", (((List<Integer>) input).get(firstIndex)), ((List<Integer>) input).get(secondIndex - 1)));
+//					firstIndex = secondIndex;
+//				}else {
+//					for (; firstIndex < secondIndex; firstIndex++) {
+//						//System.out.printf("%s,", ((List<Integer>) input).get(firstIndex));
+//						summarizedCollection.append(String.format("%s,", ((List<Integer>) input).get(firstIndex)));
+//					}
+//					
+//				}
+//			}
+//			return summarizedCollection.toString();
+//		}
 	
-	public static String summarizeCollection(Collection<Integer> input) {
-		//ArrayList<Integer> input = new ArrayList<Integer>();
-		int lengthOfInput = input.size();
-		int firstIndex = 0, secondIndex = 0;
-		//String summarizedCollection = "";
-		StringBuilder summarizedCollection = new StringBuilder();
-		
-		while (firstIndex < lengthOfInput) {
-			while ((++secondIndex < lengthOfInput) && (((List<Integer>) input).get(secondIndex) - ((List<Integer>) input).get(secondIndex -1) == 1));
-				
-			if (secondIndex - firstIndex > 2) {
-				//System.out.printf("%s-%s", ((List<Integer>) input).get(firstIndex), ((List<Integer>) input).get(secondIndex - 1));
-				summarizedCollection.append(String.format("%s-%s,", (((List<Integer>) input).get(firstIndex)), ((List<Integer>) input).get(secondIndex - 1)));
-				firstIndex = secondIndex;
-			}else {
-				for (; firstIndex < secondIndex; firstIndex++) {
-					//System.out.printf("%s,", ((List<Integer>) input).get(firstIndex));
-					summarizedCollection.append(String.format("%s,", ((List<Integer>) input).get(firstIndex)));
-				}
-				
-			}
-		}
-		return summarizedCollection.toString();
-	}
+	// Group and print out intervals
+//	public static String summarizeCollection(Collection<Integer> input) {
+//		//ArrayList<Integer> input = new ArrayList<Integer>();
+//		int lengthOfInput = input.size();
+//		int firstIndex = 0, secondIndex = 0;
+//		//String summarizedCollection = "";
+//		StringBuilder summarizedCollection = new StringBuilder();
+//		
+//		while (firstIndex < lengthOfInput) {
+//			while ((++secondIndex < lengthOfInput) && (((List<Integer>) input).get(secondIndex) - ((List<Integer>) input).get(secondIndex -1) == 1));
+//				
+//			if (secondIndex - firstIndex > 2) {
+//				//System.out.printf("%s-%s", ((List<Integer>) input).get(firstIndex), ((List<Integer>) input).get(secondIndex - 1));
+//				summarizedCollection.append(String.format("%s-%s,", (((List<Integer>) input).get(firstIndex)), ((List<Integer>) input).get(secondIndex - 1)));
+//				firstIndex = secondIndex;
+//			}else {
+//				for (; firstIndex < secondIndex; firstIndex++) {
+//					//System.out.printf("%s,", ((List<Integer>) input).get(firstIndex));
+//					summarizedCollection.append(String.format("%s,", ((List<Integer>) input).get(firstIndex)));
+//				}
+//				
+//			}
+//		}
+//		return summarizedCollection.toString();
+//	}
 	
 	// 7.24
 	
