e37ddc1888c296b9e9d6ad8e42606df585e7ecf6
Thapelo <selebedi.thapelo6@gmail.com>
2022-05-05 00:27:44 +0200

    8.9 without a sketch

--- JavaProgramming/src/Chapter08.java
+++ JavaProgramming/src/Chapter08.java
@@ -102,7 +102,244 @@ public class Chapter08 {
 //		
 //		System.out.print("Enter matrix1: ");
 //		for (int i = 0; i < )
+		
+		// 8.6
+		// Initialize two 3 x 3 matrices
+//		double[][] matrix1 = getmatrix(1);
+//		double[][] matrix2 = getmatrix(2);
+//		
+//		// Initialize matrix3 with the multiplication of matrix1 and matrix2
+//		double[][] matrix3 = multiplyMatrix(matrix1, matrix2);
+//		
+//		// Display matrix1, matrix2 and matrix3
+//		print(matrix1, matrix2, matrix3);
+		
+		// 8.7
+		
+//		double[][] points = {
+//				{-1, 0, 3},
+//				{-1, -1, -1},
+//				{4, 1, 1},
+//				{2, 0.5, 9},
+//				{3.5, 2, -1},
+//				{3, 1.5, 3},
+//				{-1.5, 4, 2},
+//				{5.5, 4, -0.5}
+//		};
+//		
+//		int p1 = 0, p2 = 1;
+//		double shortestDistance = distance(points[p1][0], points[p1][1], points[p1][2], 
+//				points[p2][0], points[p2][1], points[p2][2]);
+//		
+//		for (int i = 0; i < points.length; i++) {
+//			for (int j = i + 1; j < points.length; j++) {
+//				double distance = distance(points[i][0], points[i][1], points[i][2],
+//						points[j][0], points[j][1], points[j][2]);
+//				
+//				if (shortestDistance > distance) {
+//					p1 = i;
+//					p2 = j;
+//					shortestDistance = distance;
+//				}
+//			}
+//		}
+//		
+//		System.out.println("The closest two points are "+
+//				"(" + points[p1][0] + ", " + points[p1][1] + ", " + points[p1][2] + ") and (" +
+//				points[p2][0] + ", " + points[p2][1] + ", " + points[p2][2] + ")");
+		
+		// 8.8
+//		Scanner input = new Scanner(System.in);
+//		
+//		System.out.print("Enter the number of points: ");
+//		int numberOfPoints = input.nextInt();
+//		
+//		System.out.print("Enter " + numberOfPoints + " points: ");
+//		double[][] points = new double[numberOfPoints][2];
+//		for (int i = 0; i < points.length; i++) {
+//			for (int j = 0; j < points[i].length; j++) {
+//				points[i][j] = input.nextDouble();
+//			}
+//		}
+//		
+//		int p1 = 0, p2 = 1;
+//		double shortestDistance = distance(points[p1][0], points[p1][1], points[p2][0], 
+//				points[p2][1]);
+//		
+//		for (int i = 0; i < points.length; i++) {
+//			for (int j = i + 1; j < points.length; j++) {
+//				double distance = distance(points[i][0], points[i][1], points[j][0],
+//						points[j][1]);
+//				
+//				if (shortestDistance > distance) {
+//					p1 = i;
+//					p2 = j;
+//					shortestDistance = distance;
+//				}
+//			}
+//		}
+//		
+//		
+//		for (int i = 0; i < points.length; i++) {
+//			for (int j = i + 1; j < points.length; j++) {
+//				double distance = distance(points[i][0], points[i][1], points[j][0],
+//						points[j][1]);
+//				
+//				if (distance == shortestDistance) {
+//					System.out.println("The closest two points are "+
+//							"(" + points[i][0] + ", " + points[i][1] + ") and (" +
+//							points[j][0] + ", " + points[j][1] + ")");
+//				}
+//			}
+//		}
+//		input.close();
+//		System.out.println("Their distance is: " + shortestDistance);
+		
+		// 8.9
+		Scanner input = new Scanner(System.in);
+		
+		String[][] grid = new String[3][3];
+		
+		boolean turns = false;
+		boolean status = true;
+		
+		String player1 = "X", player2 = "O";
+		
+		while(status) {
+			var result = turns ? player1 : player2;
+			System.out.print("Enter a row (0, 1, or 2) for player " + result + " : ");
+			int row = input.nextInt();
+			System.out.print("Enter a column (0, 1, or 2) for player " + result + " : ");
+			int column = input.nextInt();
+			grid[row][column] = result;
+			
+			// Change players turns
+			if (turns == true) {
+				
+				turns = false;
+			}
+			else{
+				
+				turns = true;
+			}
+			
+			// Determining a win
+			if (grid[0][0] == result && grid[1][1] == result && grid[2][2] == result) {
+				
+				System.out.println(result + " player won");
+				status = false;
+			}
+			else if (grid[0][2] == result && grid[1][1] == result && grid[2][0] == result) {
+				
+				System.out.println(result + " player won");
+				status = false;
+			}
+			else if (grid[0][0] == result && grid[0][1] == result && grid[0][2] == result) {
+				
+				System.out.println(result + " player won");
+				status = false;
+			}
+			else if (grid[1][0] == result && grid[1][1] == result && grid[1][2] == result) {
+				
+				System.out.println(result + " player won");
+				status = false;
+			}
+			else if (grid[2][0] == result && grid[2][1] == result && grid[2][2] == result) {
+				
+				System.out.println(result + " player won");
+				status = false;
+			}
+			else if (isFull(grid)) { // Determining a draw
+				
+				System.out.println("It's a draw");
+				status = false;
+			}
+			else {
+				status = true;
+			}
+		}
+		
 	}
+	
+	// 8.9
+	
+	public static boolean isFull(String[][] grid) {
+		
+		for (int i = 0; i < grid.length; i++)
+			for (int j = 0; j < grid[i].length; j++) {
+				
+				if (grid[i][j] == null)
+					return false;
+			}
+		return true;
+	}
+	
+	// 8.8
+	
+//		public static double distance(double x1, double y1, double x2, double y2) {
+//			
+//			return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+//		}
+	
+	//8.7
+//	public static double distance(double x1, double y1, double z1, double x2, double y2, double z2) {
+//		
+//		return Math.sqrt((x2 - x1) * (y2 - y1) * (z2 - z1));
+//	}
+	
+	// 8.6
+//	public static double[][] getmatrix(int n ){
+//		
+//		Scanner input = new Scanner(System.in);
+//		
+//		System.out.print("Enter matrix" + n + ": ");
+//		
+//		double[][] m = new double[3][3];
+//		for (int i = 0; i < 3; i++) {
+//			for (int j = 0; j < 3; j++) {
+//				m[i][j] = input.nextDouble();
+//			}
+//		}
+//		
+//		return m;
+//	}
+//	
+//	public static double[][] multiplyMatrix(double[][] a, double[][] b){
+//		
+//		double[][] c = new double[3][3];
+//		int count = 0;
+//		
+//		for (int i = 0; i < c.length; i++) {
+//			for (int j = 0; j < c[i].length; j++) {
+//				for (int h = 0; h < c.length; h++) {
+//					c[i][j] += a[i][h] * b[h][j];
+//				}
+//			}
+//		}
+//		return c;
+//	}
+//	
+//	// print displays all the elements in row r
+//	public static void print(double[][] m, int r) {
+//		for (int j = 0; j < m[r].length; j++) {
+//			System.out.print(m[r][j] + " ");
+//		}
+//	}
+//	
+//	// print displays results
+//	public static void print(double[][] m1, double[][] m2, double[][] m3) {
+//		
+//		System.out.println("The matrices are added as follows");
+//		
+//		for (int i = 0; i < 3; i++) {
+//			print(m1, i);
+//			System.out.print((i == 1 ? " * " : "   "));
+//			print(m2, i);
+//			System.out.print((i == 1 ? " = " : "   "));
+//			print(m3, i);
+//			System.out.println();
+//		}
+//	}
 	
 	//8.5
 //	public static double[][] addmatrix(double[][] a, double[][] b){
